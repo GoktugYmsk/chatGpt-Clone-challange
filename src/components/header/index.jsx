8948801ce2ec920f0f2f7156b366a956098b7079
GoktugYmsk <gogtug033@outlook.com>
2024-02-13 09:49:50 +0300

    codes are formated

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { IoIosLogOut } from 'react-icons/io';
+import { IoIosLogOut } from "react-icons/io";
 
-import './index.scss'
+import "./index.scss";
 
 function Header() {
-    const [isLogin, setIslogin] = useState(false);
-    const navigate = useNavigate();
+  const [isLogin, setIslogin] = useState(false);
+  const navigate = useNavigate();
 
-    const getUserName = sessionStorage.getItem('user');
+  const getUserName = sessionStorage.getItem("user");
 
-
-    useEffect(() => {
-        const getLogin = sessionStorage.getItem('user');
-        if (getLogin) {
-            setIslogin(true);
-        }
-    }, [])
-
-    const handleLogoutClick = () => {
-        sessionStorage.removeItem('user');
-        window.location.reload();
+  useEffect(() => {
+    const getLogin = sessionStorage.getItem("user");
+    if (getLogin) {
+      setIslogin(true);
     }
+  }, []);
 
-    return (
-        <div className='container-header' >
-            {(isLogin) ? (
-                <div className='container-header__name' >
-                    <p>{getUserName}</p>
-                    <IoIosLogOut onClick={handleLogoutClick} className='icon' />
-                </div>
-            ) : (
-                <p onClick={() => navigate('/login')} className='login-message' >Giriş Yap</p>
-            )
-            }
+  const handleLogoutClick = () => {
+    sessionStorage.removeItem("user");
+    window.location.reload();
+  };
+
+  return (
+    <div className="container-header">
+      {isLogin ? (
+        <div className="container-header__name">
+          <p>{getUserName}</p>
+          <IoIosLogOut onClick={handleLogoutClick} className="icon" />
         </div>
-    )
+      ) : (
+        <p onClick={() => navigate("/login")} className="login-message">
+          Giriş Yap
+        </p>
+      )}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
